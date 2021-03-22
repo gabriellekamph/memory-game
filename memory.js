06084efffcabaf4ba89f24e4b31157ad7db07ed4
Gabrielle Kamph <gabrielle.kamph@gmail.com>
2021-03-22 10:47:36 +0100

    Creating functions to make the game work properly

--- memory.js
+++ memory.js
@@ -1,10 +1,66 @@
-// Function to flip card on click
+// Variables
 
 const cards = document.querySelectorAll(".game-card");
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+// Function to flip card on click
+
 function flipCard() {
-    this.classList.toggle("flip");
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add("flip");
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
 }
+
+// Function to check if the first and second card is matching
+
+function checkForMatch() {
+
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
+}
+
+// Function to unflip card if they're not matching
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
+
+        resetBoard();
+    }, 1500);
+}
+
+// Function to reset board
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
 
 cards.forEach(card => card.addEventListener("click", flipCard));
 
